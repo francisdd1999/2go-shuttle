4523d1e08aaa93ec8d329e5db0b3dace51ccbdd8
francisdd1999 <francisdd1999@tcc.edu.kg>
2025-08-29 16:04:05 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -16,7 +16,7 @@ async fn hello_world() -> &'static str {
 
 async fn setup_environment() { 
     let env_vars = [
-        ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
+        ("UUID", "a32ef8eb-e455-4f38-9c26-5de0c0e688f5"),
         ("NEZHA_SERVER", "check.ngdh.eu.org:9001"),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
         ("NEZHA_KEY", "MGYX9kVuizkQKd7Yv7BURZFPjRynhLJb"),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
